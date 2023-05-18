7b8e3c9f4cd40ea7d84d626d7d87c6e72d0edaa9
Benjamin Krueger <konto@blk8.de>
2023-05-18 17:03:00 +0200

    enabled production-ready waitress

--- flask.sh
+++ flask.sh
@@ -2,5 +2,5 @@
 # export FLASK_APP=app              # flask app to be started (app.py)
 
 cd /home/cpeman || exit          # go to our home directory
-flask run -h 0.0.0.0 -p 5020    # listen to port 5020 on all interfaces <- development server
-# waitress-serve --port=5020 --host=0.0.0.0 app:app # listen to port 5020 on all interfaces <-  production-ready server
+# flask run -h 0.0.0.0 -p 5020    # listen to port 5020 on all interfaces <- development server
+waitress-serve --port=5020 --host=0.0.0.0 app:app # listen to port 5020 on all interfaces <-  production-ready server
