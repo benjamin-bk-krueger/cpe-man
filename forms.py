d184220bba6f2078ecea6f58e361ad594cfbefe9
Benjamin Krueger <konto@blk8.de>
2023-06-14 21:45:27 +0200

    Small Fix: Allow 0 yearly CPE requirement

--- forms.py
+++ forms.py
@@ -106,7 +106,7 @@ class CertificationForm(FlaskForm):
     image = SelectField('Image', choices=["none"], validate_choice=False)
     organization = SelectField('Select Organization', choices=["none"], validate_choice=False)
     cycle_length = IntegerRangeField('Cycle Length', validators=[NumberRange(min=1, max=3)])
-    requirement_year = IntegerRangeField('Required each year', validators=[NumberRange(min=10, max=50)])
+    requirement_year = IntegerRangeField('Required each year', validators=[NumberRange(min=0, max=50)])
     requirement_full = IntegerRangeField('Required each cycle', validators=[NumberRange(min=10, max=150)])
 
 
